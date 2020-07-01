e992a78c104e4b54033e250def8f9b9798723a08
joaopedromata <joaopmata182@gmail.com>
2020-07-01 20:34:46 -0300

    third

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const app = express()
+const cors = require('cors')
+
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+app.use(cors())
 app.use(routes)
 
 const PORT = 3333
